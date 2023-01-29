313875969dd67f592a4d70bc0b9c08a1e6bf5b99
userword <tonifaseemo@gmail.com>
2023-01-29 17:43:57 -0600

    Working on Cup Game
    Text now displays hotkeys to play the minigames we have working so far
    
    I should probably push more frequently lol

--- Assets/BarCollisionHandler.cs
+++ Assets/BarCollisionHandler.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BarCollisionHandler : MonoBehaviour
 {
 
+    //I'm an idiot
+    //I wrote this class and then rewrote it because I thought it didnt work
+    //turns out I just didn't have the colliders set correctly...
+
     bool green, yellow, red;
 
     private void Awake()
@@ -13,12 +17,12 @@ public class BarCollisionHandler : MonoBehaviour
         green = false;
         red = false;
         yellow = false;
-
+       
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        Debug.Log("Just collided with " + collision.gameObject.name);
+        //Debug.Log("Just collided with " + collision.gameObject.name);
 
         switch (collision.gameObject.name) {
 
@@ -41,7 +45,7 @@ public class BarCollisionHandler : MonoBehaviour
     void OnTriggerExit2D(Collider2D collision)
     {
 
-        Debug.Log("Just exited with " + collision.gameObject.name);
+       // Debug.Log("Just exited with " + collision.gameObject.name);
 
         switch (collision.gameObject.name)
         {
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,14 +7,24 @@ public class GameController : MonoBehaviour
 
     public GameObject dicePrefab;
 
+    public GameObject cupGamePrefab;
+
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Q)){
 
             Instantiate(dicePrefab);
 
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            Instantiate(cupGamePrefab);
+
+        }
+
 
     }
 
