21cdcde5e163df89a83d9e4e00fdc56a6ccad167
meshbound <60497577+meshbound@users.noreply.github.com>
2023-01-29 21:22:39 -0600

    Workingish version of lockpicking game

--- Assets/Scripts/LockPicking.cs
+++ Assets/Scripts/LockPicking.cs
@@ -8,58 +8,116 @@ public class LockPicking : MonoBehaviour
 {
 
     [System.Serializable]
-    internal class LockKeyPair
+    internal class RingKeyPair
     {
-        [SerializeField] private RawImage _ring;
-        public RawImage Ring => _ring;
-        [SerializeField] private RawImage _key;
-        public RawImage Key => _key;
+        public RawImage ring;
+        public RawImage key;
+        public float angularVelocity = 90f;
     }
 
-    [SerializeField] private List<LockKeyPair> lockKeyPairs;
-    [SerializeField] private float failedCooldown;
-    private LockKeyPair current;
+    [SerializeField] private List<RingKeyPair> lockKeyPairs;
+    [SerializeField] private Slider timeBar;
+    [SerializeField] private Image timeBarFillArea;
+    [SerializeField] private float totalTime = 10f;
+    [SerializeField] private float cooldownTime = 1f;
+    [SerializeField] private float angleAllowance = 10f;
+    private RingKeyPair current => lockKeyPairs[_currentIndex];
+    private int _currentIndex;
+    private int currentIndex
+    {
+        get => _currentIndex;
+        set
+        {
+            if (value >= 0 && value <= lockKeyPairs.Count)
+            {
+                _currentIndex = value;
+                current.key.enabled = true;
+            }
+            else if (value < 0)
+                OnWin();
+        }
+    }
+    private float totalClock;
     private float cooldownClock;
+    private bool gameEnded = true;
 
     private void Start() 
     {
-        Init();
+        Initalize();
     }
 
     private void Update() 
     {
+        if (gameEnded)
+            return;
 
+        // update clocks
+        totalClock = Mathf.Clamp(totalClock - Time.deltaTime, 0, totalTime);
+        cooldownClock = Mathf.Clamp(cooldownClock - Time.deltaTime, 0, cooldownTime);
+        timeBar.value = totalClock / totalTime;
+
+        if (totalClock == 0)
+        {
+            OnLoss();
+            return;
+        }
+
+        Vector3 rotationDelta = Vector3.forward * current.angularVelocity * Time.deltaTime;
+        current.key.transform.eulerAngles += rotationDelta;
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownClock == 0)
+        {
+            
+            Color temp = current.key.color;
+            current.key.color = Color.gray * temp;
+            current.key.DOColor(temp, 0.25f);
+
+            if (StopCheck())
+            {
+                // the player successfully picked the ring
+                Vector3 ringRotation = current.ring.transform.eulerAngles;
+                current.key.transform.eulerAngles = ringRotation;
+                currentIndex--;
+            }
+            else
+            {
+                // the player failed to pick the ring
+                cooldownClock = cooldownTime;
+                // give the player some feedback
+                timeBarFillArea.DOColor(Color.red, cooldownTime / 4f).SetLoops(4, LoopType.Yoyo);
+                timeBar.transform.DOShakePosition(cooldownTime / 2f);
+            }
+        }
+    }
+
+    private void Initalize()
+    {
+        //Debug.Log(lockKeyPairs.Count);
+        foreach (RingKeyPair lockKeyPair in lockKeyPairs)
+        {
+            lockKeyPair.key.enabled = false;
+            Vector3 rotation = Vector3.forward * Random.Range(0f, 360f);
+            lockKeyPair.ring.rectTransform.eulerAngles = rotation;
+        }
+        currentIndex = lockKeyPairs.Count - 1;
+        totalClock = totalTime;
+        gameEnded = false;
     }
 
     private bool StopCheck()
     {
-        //float angle = Quaternion.Angle (a, b);
- 
-        //bool sameRotation = Mathf.Abs (angle) < 1e-3f;
-        return true;
+        float angle = Quaternion.Angle(current.key.transform.rotation, current.ring.transform.rotation);
+        return Mathf.Abs(angle) <= 10f;
     }
 
-    private void Init()
+    private void OnWin()
     {
-        foreach (LockKeyPair lockKeyPair in lockKeyPairs)
-        {
-            lockKeyPair.Key.enabled = false;
-            Vector3 rotation = Vector3.forward * Random.Range(0f, 360f);
-            lockKeyPair.Ring.rectTransform.eulerAngles = rotation;
-        }
-        StartCoroutine(PickingLoop());
-    }
-
-    private IEnumerator PickingLoop()
-    {
-        for (int i = lockKeyPairs.Count - 1; i >= 0; i--)
-        {
-            current = lockKeyPairs[i];
-            while(current != null)
-                yield return new WaitForSeconds(.1f);
-        }
-        // the player finished picking all the locks!
+        gameEnded = true;
         Debug.Log("You win!");
-        yield return null;
+    }
+
+    private void OnLoss()
+    {
+        gameEnded = true;
+        Debug.Log("You lost!");
     }
 }
