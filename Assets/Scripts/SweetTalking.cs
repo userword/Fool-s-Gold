34e154ec58af0ac515eaf49b8ca8b10302d96883
Muhammad <mahdi018@umn.edu>
2023-02-02 20:26:57 -0600

    Added Bar

--- Assets/Scripts/SweetTalking.cs
+++ Assets/Scripts/SweetTalking.cs
@@ -7,12 +7,16 @@ public class SweetTalking : MonoBehaviour
     public AudioSource musicPlay;
     public bool Play;
     public Scroller scroll;
+    public float score;
+    public int miss = 0;
+    public SpriteRenderer WinBar;
 
     public static SweetTalking instance;
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        WinBar.drawMode = SpriteDrawMode.Sliced;
     }
 
     // Update is called once per frame
@@ -25,14 +29,29 @@ public class SweetTalking : MonoBehaviour
                 musicPlay.Play();
             }
         }
+        if (miss == 5){
+            Debug.Log("Loss");
+        }
+        if (score == 250){
+            Debug.Log("WIN");
+        }
         
     }
 
     public void NoteHit() {
         Debug.Log("YASSSS");
+        score += 50;
+        WinBar.size += new Vector2(0.6f, 0f);
+        Debug.Log(score);
     }
 
     public void NoteMiss(){
         Debug.Log("BOOO");
+        miss++;
+        if (WinBar.size.x -0.6 >= 1){
+            WinBar.size -= new Vector2(0.6f, 0f);
+        }
+        Debug.Log(miss);
     }
+
 }
