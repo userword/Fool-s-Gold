e59c636135372b3a1cb50182d5c204e7533b2986
userword <tonifaseemo@gmail.com>
2023-02-19 19:15:39 -0600

    Update PirateController.cs

--- Assets/PirateController.cs
+++ Assets/PirateController.cs
@@ -165,7 +165,6 @@ public class PirateController : MonoBehaviour
 
     }
 
-    public void SetMode
     public void TurnAwayFromWall() {
 
         if (rightCollision.contact == true && movementDirection == direction.RIGHT)
