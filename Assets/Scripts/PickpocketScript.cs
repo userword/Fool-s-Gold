a56604b8d4ec81b6fb1a08f8474fbf13cd5cbfe8
DatGameh <71962500+DatGameh@users.noreply.github.com>
2023-02-20 17:07:15 -0600

    Fixed Pickpocket
    Forgot to add the initialized dice roll.

--- Assets/Scripts/PickpocketScript.cs
+++ Assets/Scripts/PickpocketScript.cs
@@ -78,7 +78,8 @@ public class PickpocketScript : MonoBehaviour
         totalClock = totalTime;
 
         //Dice roll affects the pirate's aggression and statechange levels.
-
+        stateChange = stateChangeRolls[5 - diceRoll];
+        aggression = aggressionRolls[5 - diceRoll];
     }
 
     // Update is called once per frame
