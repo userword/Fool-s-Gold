9f616937e8a934d1b65bb8f716861f79c03312a6
userword <tonifaseemo@gmail.com>
2023-02-20 18:35:21 -0600

    Final pushhhhhh

---
+++ Assets/RumSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Rum;
+
+    public float waittime, waitrange;
+
+    private void Start()
+    {
+
+        StartCoroutine(SpawnRum());
+
+    }
+    public IEnumerator SpawnRum()
+    {
+
+        int time = (int)Random.Range(waittime, waittime + waitrange);
+
+        yield return new WaitForSeconds(time);
+
+        GameObject pirate = Instantiate(Rum);
+
+        pirate.transform.position = this.transform.position;
+
+        StartCoroutine(SpawnRum());
+
+    }
+
+
+}
